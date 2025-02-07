62ac49e1c513c29d4046c261681e233cbd8d44cd
Marion McWilliams <marionmcwilliams@mac.myfiosgateway.com>
2025-02-07 07:45:16 -0500

    Chore: adds minor styling tweaks. WIP

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,25 @@ import ProjectsSection from './components/ProjectsSection';
 function App() {
   return (
     <>
-      <div id='header-section'>
-        <HeaderSection />
+      <div id='content-div' className='flex flex-col items-center w-[51rem] h-[66rem] bg-gray-300 shadow-[0px_0px_16px_-2px_rgba(0,_0,_0,_0.7)]'>
+        <div id='header-section'>
+          <HeaderSection />
+        </div>
+        <main className='flex flex-col items-center w-full'>
+          <div id='education-section' className='w-full flex'>
+            <EducationSection />
+          </div>
+          <div id='skills-section' className='w-full flex justify-center'>
+            <SkillsSection />
+          </div>
+          <div id='experience-section' className='w-full flex justify-center'>
+            <ExperienceSection />
+          </div>
+          <div id='projects-section' className='w-full flex justify-center'>
+            <ProjectsSection />
+          </div>
+        </main>
       </div>
-      <main>
-        <div id='education-section'>
-          <EducationSection />
-        </div>
-        <div id='skills-section'>
-          <SkillsSection />
-        </div>
-        <div id='experience-section'>
-          <ExperienceSection />
-        </div>
-        <div id='projects-section'>
-          <ProjectsSection />
-        </div>
-      </main>
     </>
   );
 }
--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default function EducationSection() {
     return (
         <div className="header">
-            <h1 className="text-3xl font-bold text-center text-blue-500">
-                Education Section!
-            </h1>
+            <h1 className="text-xl font-bold text-left text-black">Education</h1>
             <hr />
         </div>
     );
