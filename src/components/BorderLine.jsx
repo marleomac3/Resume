38f19f500e74bc568a747a60c90a678b765f6638
Marion McWilliams <marionmcwilliams@Marions-MacBook-Air.local>
2025-02-10 10:03:35 -0500

    Adds uniform page structure

--- src/components/BorderLine.jsx
+++ src/components/BorderLine.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function BorderLine() {
     return (
-        <div className="w-[96%]">
+        <div className="w-[96%] py-1">
             <hr className='w-[100%] mx-auto border-gray-500'/>
         </div>
     );
