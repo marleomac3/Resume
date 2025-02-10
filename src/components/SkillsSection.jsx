497c850ab90825554d69ce2ae2004d09fb1835c8
Marion McWilliams <marionmcwilliams@Marions-MacBook-Air.local>
2025-02-10 10:18:03 -0500

    Initial commit

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default function SkillsSection() {
     return (
         <div className="header">
-            <h1 className="text-3xl font-bold text-blue-500">
-                Skills Section!
-            </h1>
+            <h1 id='section-header'>Skills</h1>
         </div>
     );
 }
