c41a0714ffc49ade8e19bb2fee5cec92611784a3
Marion McWilliams <marionmcwilliams@mac.myfiosgateway.com>
2025-02-10 23:00:37 -0500

    Adds additional content

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 export default function ProjectsSection() {
     return (
-        <div className="header">
-            <h1 className="text-3xl font-bold text-blue-500">
-                Projects Section!
-            </h1>
+        <div className="w-[96%]">
+            <h1 id='section-header'>Projects</h1>
+            <ul>
+                <li>
+                    <h2 id='list-header'>Fullstack Battleship Game</h2>
+                    <li>[INSERT DETAILS HERE]</li>
+                    <li>[INSERT DETAILS HERE]</li>
+                </li>
+                <li>
+                    <h2 id='list-header'>Blackjack Game made with Python</h2>
+                    <li>[INSERT DETAILS HERE]</li>
+                    <li>[INSERT DETAILS HERE]</li>
+                </li>
+                <li>
+                    <h2 id='list-header'>Pokemon Pokedex API</h2>
+                    <li>[INSERT DETAILS HERE]</li>
+                    <li>[INSERT DETAILS HERE]</li>
+                </li>
+            </ul>
         </div>
     );
 }
