40aae933ccfd9d07119beaf7365837b46f20d72d
Marion McWilliams <marionmcwilliams@mac.myfiosgateway.com>
2025-02-18 16:51:51 -0500

    Feat: setting up mobile version

---
+++ src/versions/DesktopResume.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import HeaderSection from "../components/HeaderSection";
+import EducationSection from "../components/EducationSection";
+import SkillsSection from "../components/SkillsSection";
+import ExperienceSection from "../components/ExperienceSection";
+import ProjectsSection from "../components/ProjectsSection";
+import BorderLine from "../components/BorderLine";
+
+export default function DesktopResume() {
+  return (
+    <>
+      <div
+        id="content-div"
+        className="flex flex-col mx-auto w-[51rem] h-[66rem] bg-white iphone14:hidden">
+        <div id="header-section" className="ml-4 w-full">
+          <HeaderSection />
+          <BorderLine />
+        </div>
+        <main className="flex flex-col">
+          <div id="education-section" className="ml-4 w-full">
+            <EducationSection />
+            <BorderLine />
+          </div>
+          <div id="skills-section" className="ml-4 w-full">
+            <SkillsSection />
+            <BorderLine />
+          </div>
+          <div id="experience-section" className="ml-4 w-full">
+            <ExperienceSection />
+            <BorderLine />
+          </div>
+          <div id="projects-section" className="ml-4 w-full">
+            <ProjectsSection />
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
