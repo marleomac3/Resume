c91e460aec0e310fdfdfca8f5c1b14e20a063b25
Marion McWilliams <marionmcwilliams@mac.myfiosgateway.com>
2025-02-18 20:58:52 -0500

    feat: adds buttons for phone num and email

--- src/components/iphone14/HeaderSection.jsx
+++ src/components/iphone14/HeaderSection.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import CallButton from "./CallButton";
+import EmailButton from "./EmailButton";
 
 export default function HeaderSection() {
     return (
@@ -6,14 +8,17 @@ export default function HeaderSection() {
             <h1 className="text-2xl font-bold text-center text-black">
                 Marion L. McWilliams III
             </h1>
-            <div className='flex text-xs gap-[6px]'>
-                <p>302-563-0316</p>
-                <p>|</p>
-                <p><a href="mailto:marionlmcwilliams@gmail.com?subject=Portfolio%20Inquiry&body=Hello%20Marion%2C%20I%20have%20a%20question%20about%20your%20portfolio.%20" target='_blank' className='text-blue-700 underline cursor-pointer'>marionlmcwilliams@gmail.com</a></p>
-                <p>|</p>
-                <p><a href='https://www.linkedin.com/in/marion-mcwilliams/' target='_blank' className='text-blue-700 underline cursor-pointer'>https://www.linkedin.com/in/marion-mcwilliams/</a></p>
-                <p>|</p>
-                <p><a href='https://github.com/marleomac3' target='_blank' className='text-blue-700 underline cursor-pointer'>https://github.com/marleomac3</a></p>
+            <div className="flex justify-around w-full gap-1">
+                <div className="w-1/2">
+                    <CallButton phoneNumber="3025630316" />
+                </div>
+                <div className="w-1/2">
+                    <EmailButton
+                        email="marionlmcwilliams@gmail"
+                        subject="Resume Inquiry"
+                        message="Hello Marion, I have a question about your resume."
+                    />
+                </div>
             </div>
         </div>
     );
